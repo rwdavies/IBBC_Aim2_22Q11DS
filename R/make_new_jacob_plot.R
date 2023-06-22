832588de021e0dd2c9597754327cab66f9813639
rwdavies <robertwilliamdavies@gmail.com>
2023-06-22 16:39:27 +0100

    changes

--- R/make_new_jacob_plot.R
+++ R/make_new_jacob_plot.R
@@ -2,6 +2,13 @@
 ## but for arbitrary percent variance explained, thresholds, etc
 
 
+## checks
+prs_explains_var <- 0.05
+a <- sqrt(prs_explains_var)
+b <- sqrt(1 - a ** 2)
+var(a * rnorm(10000) + b * rnorm(10000))
+
+
 get_ppv <- function(
     pop_prev = 0.01,
     prs_explains_var = 0.05,
@@ -19,7 +26,7 @@ get_ppv <- function(
     ## a is the sqrt of prs_explains_var
     ## and b is sqrt of 1 - prs_explains_var
     a <- sqrt(prs_explains_var)
-    b <- sqrt(1 - prs_explains_var)
+    b <- sqrt(1 - a ** 2)
     ## now suppose we know someone has a PRSile of some value, say ile_thresh e.g. 0.10
     ## so this gives Y_1 = qnorm(ile_thresh) = c
     c <- qnorm(ile_thresh)
@@ -30,32 +37,42 @@ get_ppv <- function(
     ## Y_2 > (k - a * c) / b
     ## which is
     ppv <- 1 - pnorm(q = (k - a * c) / b)
-    ppv
+    ## note we expect the PS to be neutral when
+    neutral_ile <- pnorm(k * (1 - b) / a)
+    return(c(ppv, neutral_ile))
 }
 
 ## choose some cutoffs
 
 make_prevalence_focused_plot <- function(
     pop_prev,
-    col = "black"
+    col = "black",
+    x = NA
 ) {
     ## specify which of these we are interested in
     prs_explains_vars <- c(0.025, 0.05, 0.075, 0.10, 0.15, 0.20)
     ## similarly for the psiles
-    xx <- log(50, 2) + -4:-1
-    x <- 2 ** (xx)
-    iles <- c(x, 50, 100 - x[length(x):1]) / 100
-    plot_x <- log(50, 2) + -4:4
+    if (is.na(x[1])) {
+        xx <- log(50, 2) + -4:-1
+        x <- 2 ** (xx)
+        x <- c(x, 50, 100 - x[length(x):1]) / 100
+        at_x <- log(50, 2) + -4:4
+        is_log2 <- TRUE
+    } else {
+        at_x <- x ## hope this is OK!
+        is_log2 <- FALSE
+    }
+
     ##
-    xlim <- range(plot_x)
-    ylim <- c(0, 0.5)
+    xlim <- range(at_x)
+    ylim <- c(0, 1.0)
     plot(x = 0, y = 0, xlim = xlim, ylim = ylim, axes = FALSE, col = "white", xlab = "PSile value", ylab = "PPV", main = paste0("Prevalence = ", pop_prev))
-    axis(1, labels = FALSE, tick = TRUE, at = plot_x)
+    axis(1, labels = FALSE, tick = TRUE, at = at_x)
     ## axis(1, at = plot_x, labels = round(iles * 100, 2), srt = 45)
     text(
-        x = plot_x,
+        x = at_x,
         y = ylim[1] - 0.06,
-        labels = round(iles * 100, 2),
+        labels = round(x * 100, 2),
         xpd = NA,
         srt = 35
     )
@@ -64,29 +81,44 @@ make_prevalence_focused_plot <- function(
     ##
     for(i_prs_explains_vars in 1:length(prs_explains_vars)) {
         prs_explains_var <- prs_explains_vars[i_prs_explains_vars]
-        vals <- sapply(iles, function(ile_thresh) {
-            get_ppv(
+        vals <- sapply(x, function(ile_thresh) {
+            out <- get_ppv(
                 pop_prev = pop_prev,
                 prs_explains_var = prs_explains_var,
                 ile_thresh = ile_thresh
             )
         })
-        print(vals)
-        lines(x = plot_x, vals, lwd = lwds[i_prs_explains_vars], col = col)
+        neutral <- vals[2, 1]
+        vals <- vals[1, ]
+        lines(x = at_x, vals, lwd = lwds[i_prs_explains_vars], col = col)
+        if (is_log2) {
+            if (neutral < 0.5) {
+                p <- log(100 * neutral, 2)
+            } else {
+                p <- 2 * log(50, 2) - log(100 * (1 - neutral), 2)
+            }
+        } else {
+            ## plot directly
+            p <- neutral
+        }
+        points(x = p, y = pop_prev, pch = 4, col = col)
+        ## check neutral
     }
     legend("topleft", lwd = lwds, legend = prs_explains_vars)
+    abline(h = pop_prev, lwd = 2, col = col)
 }
 
 
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-pdf("~/Downloads/for_jacob.pdf", height = 10, width = 5)
-par(mfrow = c(2, 1))
-make_prevalence_focused_plot(
-    pop_prev = 0.02,
-    col = cbPalette[2]
-)
-make_prevalence_focused_plot(
-    pop_prev = 0.2,
-    col = cbPalette[4]
-)
+pdf("~/Downloads/for_jacob.pdf", height = 12, width = 8)
+par(mfrow = c(3, 2))
+pop_prevs <- c(0.02, 0.05, 0.10, 0.25, 0.40, 0.60)
+for(i_pop_prev in 1:length(pop_prevs)) {
+    make_prevalence_focused_plot(
+        pop_prev = pop_prevs[i_pop_prev],
+        col = cbPalette[i_pop_prev]
+    )
+}
 dev.off()
+
+
