433a59036bbd5ed1e540787812ed2d18e5062995
rwdavies <robertwilliamdavies@gmail.com>
2023-06-22 15:25:13 +0100

    some changes

--- R/make_new_jacob_plot.R
+++ R/make_new_jacob_plot.R
@@ -36,7 +36,8 @@ get_ppv <- function(
 ## choose some cutoffs
 
 make_prevalence_focused_plot <- function(
-    pop_prev
+    pop_prev,
+    col = "black"
 ) {
     ## specify which of these we are interested in
     prs_explains_vars <- c(0.025, 0.05, 0.075, 0.10, 0.15, 0.20)
@@ -45,30 +46,47 @@ make_prevalence_focused_plot <- function(
     x <- 2 ** (xx)
     iles <- c(x, 50, 100 - x[length(x):1]) / 100
     plot_x <- log(50, 2) + -4:4
-    ## 
+    ##
     xlim <- range(plot_x)
     ylim <- c(0, 0.5)
-    plot(x = 0, y = 0, xlim = xlim, ylim = ylim, axes = FALSE, col = "white")    
-    axis(1, at = plot_x, labels = round(iles * 100, 2))
+    plot(x = 0, y = 0, xlim = xlim, ylim = ylim, axes = FALSE, col = "white", xlab = "PSile value", ylab = "PPV", main = paste0("Prevalence = ", pop_prev))
+    axis(1, labels = FALSE, tick = TRUE, at = plot_x)
+    ## axis(1, at = plot_x, labels = round(iles * 100, 2), srt = 45)
+    text(
+        x = plot_x,
+        y = ylim[1] - 0.06,
+        labels = round(iles * 100, 2),
+        xpd = NA,
+        srt = 35
+    )
     axis(2)
-    lwds <- seq(1, 3, length(prs_explains_vars))
-    ## 
+    lwds <- seq(1, 3, length.out = length(prs_explains_vars))
+    ##
     for(i_prs_explains_vars in 1:length(prs_explains_vars)) {
         prs_explains_var <- prs_explains_vars[i_prs_explains_vars]
         vals <- sapply(iles, function(ile_thresh) {
             get_ppv(
                 pop_prev = pop_prev,
-                prs_explains_var = 0.05,
+                prs_explains_var = prs_explains_var,
                 ile_thresh = ile_thresh
             )
         })
-        lines(x = plot_x, vals, lwd = lwds[i_prs_explains_vars])
+        print(vals)
+        lines(x = plot_x, vals, lwd = lwds[i_prs_explains_vars], col = col)
     }
+    legend("topleft", lwd = lwds, legend = prs_explains_vars)
 }
 
 
-
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+pdf("~/Downloads/for_jacob.pdf", height = 10, width = 5)
+par(mfrow = c(2, 1))
 make_prevalence_focused_plot(
-    prs_explains_vars = prs_explains_vars,
-    pop_prev = 0.01
+    pop_prev = 0.02,
+    col = cbPalette[2]
 )
+make_prevalence_focused_plot(
+    pop_prev = 0.2,
+    col = cbPalette[4]
+)
+dev.off()
